df6efd935d7226466c048b919ca05376c8a623fd
Lucimara Souza <lucimarasouzah@gmail.com>
2022-10-15 11:32:34 -0300

    Deploy

---
+++ src/components/Input.stories.tsx
@@ -0,0 +1,11 @@
+import { TextInput, TextInputProps } from "./Input";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "Components/Button",
+  component: TextInput,
+  args: {},
+  argTypes: {},
+} as Meta<TextInputProps>;
+
+export const Default: StoryObj<TextInputProps> = {};
---
+++ src/components/Input.tsx
@@ -0,0 +1,7 @@
+import { clsx } from "clsx";
+
+export interface TextInputProps {}
+
+export function TextInput({}: TextInputProps) {
+  return <input className={clsx("py-4 px-3 bg-gray-800 rounded text-gray-100 text-xs placeholder:text-gray-400 w-full")} />;
+}
